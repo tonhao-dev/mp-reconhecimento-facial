3540a2bb52c261444ce6d441c49023090532d816
Luis Santiago <luis.santiago@techbiz.com.br>
2026-01-12 19:00:40 -0500

    move a logica de carregamento dos modelos para um hook personalizado

--- src/hooks/useLoadModels.tsx
+++ src/hooks/useLoadModels.tsx
@@ -1,1 +1,16 @@
-export function useLoadModels() {}
+import { useEffect } from "react";
+import * as faceapi from "face-api.js";
+
+export function useLoadModels() {
+  async function initializeRecognitionModels() {
+    await Promise.all([
+      faceapi.loadTinyFaceDetectorModel("/models"),
+      faceapi.loadFaceLandmarkModel("/models"),
+      faceapi.loadFaceExpressionModel("/models"),
+    ]);
+  }
+
+  useEffect(() => {
+    initializeRecognitionModels;
+  }, []);
+}
